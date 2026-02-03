ab42200f98c5273e1c0138a89228255a907ec3be
arvind021 <ak4392916@gmail.com>
2026-02-03 15:06:02 +0530

    Update bot token and admin ID

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # ================== CONFIG ==================
-BOT_TOKEN = "8471327440:AAFRh_dl-nQwgzh7FLOOY8VJ05K8cAU4HMo"  # @BotFather se token
-ADMIN_ID =  7875650103  # yaha apna Telegram numeric ID (int)
+BOT_TOKEN = "8298040944:AAHX8bPaCefIBq8uJxCTc7WuS3sL1bh541U"  # @BotFather se token
+ADMIN_ID =  8525538455  # yaha apna Telegram numeric ID (int)
 
 PLANS_FILE = "plans.json"
 
